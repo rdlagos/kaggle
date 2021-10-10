02ee9aa99f3a3467e44764723448f73b3d826b87
RD Lagos <ralphdavidrustialagos@gmail.com>
2021-10-10 19:36:01 +0800

    first commit - rock paper scissors

--- kaggle rock-paper-scissor agents/main.py
+++ kaggle rock-paper-scissor agents/main.py
@@ -2,6 +2,6 @@ from kaggle_environments import make
 import random
 
 env = make("rps", configuration={"episodeSteps": 1000})
-env.run(['random_agent.py', 'copy_agent.py'])
+env.run(['random_agent.py', 'copy_opponent_agent.py'])
 
 env.render(mode="ipython", width=000, height=000)
